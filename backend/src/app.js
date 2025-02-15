f69d6fc2bb294e6fcfbb018f33cefd19c8b72a2e
Mohitd45 <mohit.dhangar88@gmail.com>
2025-02-15 23:52:39 +0530

    Complaint Management System Added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import complaintRoutes from "./routes/complaint.routes.js";
 
 
 const app = express();
 app.use(express.json());
+app.use(
+    cors({
+        origin: 'http://localhost:5173', // Allow frontend origin
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+        allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+        credentials: true, // Allow cookies/auth headers if needed
+    })
+);
+app.use(cookieParser());
 
+app.use("api/v1/complaints", complaintRoutes);
 
 export default app;
--- backend/src/routes/complaint.routes.js
+++ backend/src/routes/complaint.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { submitComplaint } from "../controllers/complaint.controller";
+import { submitComplaint } from "../controllers/complaint.controller.js";
+import { authenticateUser } from "../middlewares/user.middleware.js";
 
 const router = Router();
 
-router.route("/submit-complaint").post(submitComplaint)
+router.route("/submit-complaint").post(authenticateUser, submitComplaint);
 
 export default router;
