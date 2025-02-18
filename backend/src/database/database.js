02ae21f014b31ff9f413e40eac2dae7707b8131e
Mohitd45 <mohit.dhangar88@gmail.com>
2025-02-19 02:49:57 +0530

    seed data

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB connected!! DB Host : ${connectionInstance.connection.host}`)
+        
     } catch (error) {
         console.log("mongoDB connection error : ", error);
         process.exit(1);
