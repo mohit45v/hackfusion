fd18911e344a00ac1acde3628b324d9d56c14c35
samir suroshe <sameersuroshe50@gmail.com>
2025-09-07 19:15:53 +0530

    Remove unused image files and clean up auth middleware by removing console log

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -6,7 +6,6 @@ import { User } from "../models/user.model.js";
 const verifyJwt = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-        console.log(token);
     
         if(!token){
             throw new ApiError(401, "Unauthorised request");
